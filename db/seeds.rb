e5b7a4d69f2bd5d0335e9ace8990a0bd08b3c9c8
Meghan Miller <mille265@gmail.com>
2019-06-12 09:56:27 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Artist.destroy_all
+Song.destroy_all
+
+yvette = Artist.create(name: "Yvette Young")
+mermaid = Song.create(title: "Mermaid", artist_id: yvette.id)
